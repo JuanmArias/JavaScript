171dd68253cfad021ac380432d8fe5fd5c591268
JuanmArias <juanma-adtr@hotmail.com>
2021-09-11 02:30:30 -0300

    mucho mejor

--- main.js
+++ main.js
@@ -15,13 +15,15 @@ class Auto {
     //funcion que deriva en el precio final
     cotizador(){
         const premio = this.prima * this.tarifaOrigen;
-        const precioCuota = premio / 6;        
-        let titulo = document.createElement("div");
-        titulo.innerHTML =` <h2>Cotizacion de ${marca} ${modelo}</h2>
-                            <h3>Suma Asegurada: ${this.sumaAsegurada}</h3>
-                            <h3>Valor del premio en poliza semestral: ${premio}</h3>
-                            <h3>Realizando financiacion en 6 cuotas: ${precioCuota}</h3> `
-        document.body.appendChild(titulo);
+        const precioCuota = premio / 6;
+        document.getElementById("presupuesto").remove();        
+        let presupuestofinal = document.createElement("div");
+        presupuestofinal.className = "container text-center";
+        presupuestofinal.innerHTML =` <h2>Cotizacion de ${this.nombre} </h2>
+                            <h3>Suma Asegurada:${this.sumaAsegurada} </h3>
+                            <h3>Valor del premio en poliza semestral:${premio} </h3>
+                            <h3>Realizando financiacion en 6 cuotas ${precioCuota}</h3> `
+        document.body.appendChild(presupuestofinal);
     
     }
 
@@ -48,68 +50,42 @@ const misAutos = {
     ]
 }
 
-/* //interactuamos mediante prompt para determinar que Auto quiere cotizar.
-let marca = prompt("Bienvenido al Cotizador, Especificar marca de su vehiculo: VOLKSWAGEN, FIAT, CHEVROLET\nPARA SALIR INGRESE 'ESC'").toLowerCase();
-while(marca != "ESC"){
-    if(misAutos[marca]){
-        const modelosDisponibles = misAutos[marca].map(auto => auto.nombre);
-        modelo = prompt(`AUTOMOTORES. Especificar modelo de ${marca}: ${modelosDisponibles.join(', ')}`).toLowerCase();
-        if(modelosDisponibles.find(mod => modelo === mod.toLowerCase())){
-            misAutos[marca].filter(auto => auto.nombre.toLowerCase() === modelo)[0].cotizador();
-        }else{
-            alert('Por favor ingrese una opción válida.');
-        }
-    }else{
-        alert('Por favor ingrese una opción válida. Los vehiculos cotizables son aquellos que se detallan en la lista');
-    }
-    marca = prompt(" Ingrese 'ESC' para visualizar su presupuesto , en caso de error ingrese marca correcta: Volkswagen, Fiat o Chevrolet ");
-} */
-
 const aceptar = document.getElementById("aceptar1");
 aceptar.addEventListener("click", respuestaClick)
 function respuestaClick() {
-    let nombre = document.getElementById("nombre1").value;
+    const nombre = document.getElementById("input1").value;
     localStorage.setItem("nombre", nombre)
-    let localidad = document.getElementById("localidad1").value;
+    const localidad = document.getElementById("input2").value;
     localStorage.setItem("localidad", localidad)
 
-    let cambiazo = document.getElementById("presupuesto");
-    cambiazo.parentNode.removeChild(cambiazo);
-
-    let titulo = document.createElement("div");
-    titulo.className = "container text-center";
-        titulo.innerHTML =` <h1>Bienvenido al Cotizador del Barto</h1>
-                            <h2>Marcas disponibles de cotizacion: ${Object.keys(misAutos).join(", ")}</h2>
-                            
-                            <div class="row justify-content-center">
-                                <div class="input-group mb-3 w-75">
-                                    <span class="input-group-text" id="basic-addon1">Marca</span>
-                                    <input type="text" class="form-control" id="marca1" placeholder="Ingrese aqui la marca del vehiculo" aria-label="Username" aria-describedby="basic-addon1">
-                                </div>
-                            </div> 
-                            
-                            <div class="d-grid gap-2 d-md-block">
-                                <button class="btn btn-primary btn-lg" id="siguiente1" type="button">Siguiente</button>
-                                <button class="btn btn-primary btn-lg" type="button">Cancelar</button>
-                            </div>`
-        document.body.appendChild(titulo);
+    document.getElementById("dato2").remove();
+    document.getElementById("dato1").innerHTML = `<div class="input-group mb-3 w-75">
+                                                    <span class="input-group-text" id="basic-addon1 texto1">Marca</span>
+                                                    <input type="text" class="form-control" id="input3" placeholder="Ingrese Marca de vehiculo disponible" aria-label="Username" aria-describedby="basic-addon1">
+                                                 </div>`;
+    document.getElementById("titulos").appendChild(document.createElement("h3")).innerHTML=`Marcas disponibles: ${Object.keys(misAutos).join(", ")}`;
+    aceptar.addEventListener("click", marcaClick)
 }
 
-let marca = document.getElementById("marca1").value;
-while(marca != "ESC"){
+function marcaClick() {
+    const marca = document.getElementById("input3").value.toLowerCase();
     if(misAutos[marca]){
-        
         const modelosDisponibles = misAutos[marca].map(auto => auto.nombre);
-        modelo = prompt(`AUTOMOTORES. Especificar modelo de ${marca}: ${modelosDisponibles.join(', ')}`).toLowerCase();
-        
-        
-        if(modelosDisponibles.find(mod => modelo === mod.toLowerCase())){
+        document.getElementsByTagName("h3")[0].innerHTML = `Modelos cotizables de ${marca}: ${modelosDisponibles.join(', ')}`;
+        document.getElementById("dato1").innerHTML = `<div class="input-group mb-3 w-75">
+                                                        <span class="input-group-text" id="basic-addon1 texto1">Modelo</span>
+                                                        <input type="text" class="form-control" id="input4" placeholder="Ingrese Modelo de vehiculo disponible" aria-label="Username" aria-describedby="basic-addon1">
+                                                     </div>`;
+        aceptar.addEventListener("click", modeloClick)
+        function modeloClick() {
+            const modelo = document.getElementById("input4").value.toLowerCase();                                             
+            if(modelosDisponibles.find(mod => modelo === mod.toLowerCase())){
             misAutos[marca].filter(auto => auto.nombre.toLowerCase() === modelo)[0].cotizador();
-        }else{
-            alert('Por favor ingrese una opción válida.');
+            }else{
+                alert('Por favor ingrese una opción válida.');
+            }
         }
-    }else{
-        alert('Por favor ingrese una opción válida. Los vehiculos cotizables son aquellos que se detallan en la lista');
-    }
-    marca = prompt(" Ingrese 'ESC' para visualizar su presupuesto , en caso de error ingrese marca correcta: Volkswagen, Fiat o Chevrolet ");
+        }else{
+            alert('Por favor ingrese una opción válida. Los vehiculos cotizables son aquellos que se detallan en la lista');
+        }
 }
